31c310c4cd1d392b7fd551b830e8e58840998f50
Karim <hmrn.karim@gmail.com>
2022-10-02 14:36:27 +0200

    Added dependencies : Player, Apollo. | Wrote first query and got playlist data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,36 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+//Apollo Client
+import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+    },
+  };
+});
+
+const httpLink = new HttpLink({
+  uri: process.env.SPOTIFY_URL
+});
+
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+  ssrMode: typeof window === 'undefined',
+});
+
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<ApolloProvider client={client}>
+    <Component {...pageProps} />
+  </ApolloProvider>
+  )
 }
 
 export default MyApp
