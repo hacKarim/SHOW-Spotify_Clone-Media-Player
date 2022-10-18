f84403b7e87579ebbb1c6a8be9a9916ee70238ea
Karim <hmrn.karim@gmail.com>
2022-10-18 14:47:47 +0200

    Fixed Empty playlist Placeholder

--- components/playlist/playlist.tsx
+++ components/playlist/playlist.tsx
@@ -10,27 +10,33 @@ export const Playlist = MotionHoc((props: any): ReactElement => {
 
   return (
     <>
-      <div className={styles.row_header}>
-        <div className={styles.item_image} style={{ width: 64 }}></div>
-        <div className={styles.item_trackname}>
-          <Text h4>Title</Text>
-        </div>
+      {props.showOnlyFavorites && props.favorites.favlist.length > 0 ? (
+        <div className={styles.row_header}>
+          <div className={styles.item_image} style={{ width: 64 }}></div>
+          <div className={styles.item_trackname}>
+            <Text h4>Title</Text>
+          </div>
 
-        <div className={styles.item_album}>
-          <Text h4>Album</Text>
+          <div className={styles.item_album}>
+            <Text h4>Album</Text>
+          </div>
+          <div className={styles.item_artists}>
+            <Text h4>Artists</Text>
+          </div>
+          <div className={styles.item_duration}>
+            <Text></Text>
+          </div>
+          <div className={styles.item_fav}>
+            <Text>
+              <FiHeart size={"2em"} opacity={0.3} />
+            </Text>
+          </div>
         </div>
-        <div className={styles.item_artists}>
-          <Text h4>Artists</Text>
-        </div>
-        <div className={styles.item_duration}>
-          <Text></Text>
-        </div>
-        <div className={styles.item_fav}>
-          <Text>
-            <FiHeart size={"2em"} opacity={0.3} />
-          </Text>
-        </div>
-      </div>
+      ) : (
+        props.showOnlyFavorites && (
+          <Text blockquote>You didn't like any music</Text>
+        )
+      )}
       {playlist.tracks.map((element: any, index: any) =>
         props.showOnlyFavorites ? (
           props.favorites.favlist[element.track.id].isFav && (
